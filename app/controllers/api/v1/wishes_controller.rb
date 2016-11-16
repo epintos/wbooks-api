0345c271a4d98f348813849bae933d60539d8ef0
Francisco Landino <plandino@hotmail.com>
2016-11-16 17:31:10 -0300

    Show action implemented.

--- app/controllers/api/v1/wishes_controller.rb
+++ app/controllers/api/v1/wishes_controller.rb
@@ -4,6 +4,16 @@ module Api
       def index
         render json: current_user.wishes.page(params[:page])
       end
+
+      def show
+        render json: wish
+      end
+
+      private
+
+      def wish
+        @wish ||= current_user.wishes.find(params[:id])
+      end
     end
   end
 end
