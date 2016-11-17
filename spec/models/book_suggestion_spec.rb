ed2496cfebbf5d964f9a6bb96f8472fce8cde1eb
Katrine Poulsen <katrine.poulsen@wolox.com.ar>
2016-11-17 13:15:01 -0300

    test for models

--- spec/models/book_suggestion_spec.rb
+++ spec/models/book_suggestion_spec.rb
@@ -5,20 +5,20 @@ describe BookSuggestion, type: :model do
   it { should validate_presence_of(:author) }
   it { should validate_presence_of(:link) }
 
-  subjet(:book_suggestion) do
+  subject(:book_suggestion) do
     BookSuggestion.new(
       title: title, author: author, link: link, editorial: editorial,
-      price: price, publisher: publisher, year: year.
+      price: price, publisher: publisher, year: year
     )
   end
 
-  let(:title) { Fake::Book.title }
-  let(:author) { Fake::Book.author }
-  let(:publisher) { Fake::Book.publisher }
-  let(:link) { Fake::Internet.url}
+  let(:title) { Faker::Book.title }
+  let(:author) { Faker::Book.author }
+  let(:publisher) { Faker::Book.publisher }
+  let(:link) { Faker::Internet.url}
   let(:price) { Faker::Commerce.price }
   let(:editorial) { Faker::Company.name }
+  let(:year) { Faker::Date.between(50.years.ago, 1.year.ago).year }
 
   it { is_expected.to be_valid }
-  end    
 end
