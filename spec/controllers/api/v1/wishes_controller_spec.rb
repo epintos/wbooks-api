e8ef86d2c0ae4d832620800b3ccfbf208013f140
Francisco Landino <plandino@hotmail.com>
2016-11-17 14:25:39 -0300

    Wishes controler's tests implemented.

--- spec/controllers/api/v1/wishes_controller_spec.rb
+++ spec/controllers/api/v1/wishes_controller_spec.rb
@@ -1,23 +1,66 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::WishesController, type: :controller do
+describe Api::V1::WishesController, type: :controller do
+  include_context 'Authenticated User'
 
-  # it "responds successfully with an HTTP 200 status code" do
-  #   get 'index'
-  #   expect(response).to be_success
-  #   expect(response).to have_http_status(200)
-  # end
+  describe 'GET #index' do
+    let!(:wishes) { create_list(:wish, 3, user: user) }
+    context 'When fetching all users wishes' do
+      before do
+        get :index, params: { id: user.id }
+      end
 
-  # it "displays the user's username after successful login" do
-  #   user = User.create!(email: "jdoe@hotmail.com", password: "secret", first_name: "joe", last_name: "doe")
-  #   get "/login"
-  #   assert_select "form.login" do
-  #     assert_select "input[name=?]", "username"
-  #     assert_select "input[name=?]", "password"
-  #     assert_select "input[type=?]", "submit"
-  #   end
-  #
-  #   post "/login", :email => "jdoe@hotmail.com", :password => "secret"
-  #   assert_select ".header .username", :text => "jdoe@hotmail.com"
-  # end
+      it 'responses with alls users wishes json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          wishes, each_serializer: WishSerializer
+        ).to_json
+        expect(response_body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:wish) { create(:wish, user: user) }
+    context 'When fetching a user\'s wish' do
+      before do
+        get :show, params: { user_id: user.id, id: wish.id }
+      end
+
+      it 'responses with the user\'s wishes json' do
+        expect(response_body.to_json).to eq WishSerializer.new(
+          wish, root: false
+        ).to_json
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'POST #create', type: :controller do
+    context 'When creating a user\'s wish' do
+      let(:new_wish) { build(:wish, user: user) }
+      it 'creates a new wish' do
+        expect do
+          post :create, params: {
+            id: user.id,
+            wish: { book_id: new_wish.book.id, user_id: new_wish.user.id }
+          }
+        end.to change { Wish.count }.by(1)
+      end
+
+      it 'responds with 201 status' do
+        post :create, params: {
+          id: user.id,
+          wish: { book_id: new_wish.book.id, user_id: new_wish.user.id }
+        }
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
 end
--- spec/models/wish_spec.rb
+++ spec/models/wish_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Wish, type: :model do
-  it "should create a new wish" do
+  it 'should create a new wish' do
     Wish.create user_id: 1, book_id: 10
     expect(Wish.count).to eq(1)
   end
 
-  it "should create different rows with different books" do
+  it 'should create different rows with different books' do
     Wish.create user_id: 1, book_id: 10
     Wish.create user_id: 1, book_id: 15
     expect(Wish.count).to eq(2)
   end
 
-  it "should create different rows with different users" do
+  it 'should create different rows with different users' do
     Wish.create user_id: 1, book_id: 10
     Wish.create user_id: 2, book_id: 10
     expect(Wish.count).to eq(2)
@@ -25,10 +25,10 @@ RSpec.describe Wish, type: :model do
   end
 
   it "shouldn't be valid if no user is specified" do
-    expect(Wish.create book_id: 10).to_not be_valid
+    expect(Wish.create(book_id: 10)).to_not be_valid
   end
 
   it "shouldn't be valid if no book is specified" do
-    expect(Wish.create user_id: 1).to_not be_valid
+    expect(Wish.create(user_id: 1)).to_not be_valid
   end
 end
