d03df8ec37bf901faca9b220008eead8bce9e582
Katrine Poulsen <katrine.poulsen@wolox.com.ar>
2016-11-21 11:18:44 -0300

    Adds Mailer for new Book Suggestions

---
+++ app/mailers/book_suggestion_mailer.rb
@@ -0,0 +1,14 @@
+class BookSuggestionMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.book_suggestion_mailer.new_book_suggestion_notification.subject
+  #
+
+  def new_book_suggestion_notification(book_suggestion)
+    @book_suggestion = book_suggestion
+
+    mail(to: book_suggestion.user.email, subject: 'You made a new suggestion')
+  end
+end
