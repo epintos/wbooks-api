a2fbd31fc398d6328006ba63a61705af90ad2f52
Francisco Landino <plandino@hotmail.com>
2016-11-24 14:57:43 -0300

    Adds the internationalization for english and spanish. Now I've to make the dropdown menu to choose the language.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,13 @@ class ApplicationController < ActionController::Base
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :null_session
   before_action :current_user, :authenticate_request
+  before_action :set_locale
 
   private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def user_not_authorized
     render json: { error: 'You are not authorized.' }, status: :unauthorized
