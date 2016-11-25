9820a2c25958c8987e5445c3b330ed37b799ec7a
Francisco Landino <plandino@hotmail.com>
2016-11-25 18:10:54 -0300

    Adds javascript for the communication between the browser and the server. Avoids rendering the view each time you submit a suggestion.

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -2,6 +2,4 @@ class BookSuggestion < ApplicationRecord
   belongs_to :user
 
   validates :author, :title, :link, presence: true
-
-  PERMITTED_PARAMS = [:title, :editorial, :price, :author, :link, :publisher, :year]
 end
