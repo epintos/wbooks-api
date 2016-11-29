80a5a0bb66b0d13b22ee95e2f4d69eeaac7a05d9
Katrine Poulsen <katrine.poulsen@wolox.com.ar>
2016-11-29 14:22:48 -0300

    fixes rubocop

--- app/mailers/book_suggestion_mailer.rb
+++ app/mailers/book_suggestion_mailer.rb
@@ -1,11 +1,9 @@
 class BookSuggestionMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.book_suggestion_mailer.new_book_suggestion_notification.subject
   #
-
   def new_book_suggestion_notification(book_suggestion)
     @book_suggestion = book_suggestion
 
