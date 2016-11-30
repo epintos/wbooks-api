fa40072913f7c715951b558b7eb5b4dc58de6684
Katrine Poulsen <katrine.poulsen@wolox.com.ar>
2016-11-30 11:14:07 -0300

    deleted table wish_list in migration and added depend: :destroy to comments in book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   mount_uploader :image, ImageUploader
   validates :author, :title, :genre, :publisher, :year, presence: true
