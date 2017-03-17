9620e19c40f575bdb89aab2535fdb3b7dcd025b1
Martin Montenegro <martin.92m@gmail.com>
2017-03-17 16:57:01 -0300

    Authentication imrpovements * Remove renew before warning validation * Use 64 chards for renew id token * Replace Devise#friendly_token with ruby code

--- app/authentication/authentication_unique_token.rb
+++ app/authentication/authentication_unique_token.rb
@@ -4,5 +4,10 @@ class AuthenticationUniqueToken
     def generate
       SecureRandom.hex(16)
     end
+
+    def generate_friendly(length = 32)
+      rlength = (length * 3) / 4
+      SecureRandom.urlsafe_base64(rlength).tr('lIO0', 'sxyz')
+    end
   end
 end
