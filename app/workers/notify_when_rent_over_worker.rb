0b9d71ea534da04745680473e438a3ce11061423
mtrucco <truccomarcos@gmail.com>
2017-06-22 16:17:24 -0300

    Deleted information attribute, which is no longer used, from tests. Added body to serializer

--- app/workers/notify_when_rent_over_worker.rb
+++ app/workers/notify_when_rent_over_worker.rb
@@ -3,14 +3,14 @@ class NotifyWhenRentOverWorker
 
   def perform
     rents = Rent.where(to: Time.zone.today)
-    rents.each do |rent|
+    rents.find_each do |rent|
       create_notifications(rent)
     end
   end
 
   def create_notifications(rent)
-    rent.book.users.each do |user|
-      Notification.create(reason: :updated, action_type: 'end',
+    rent.book.users.find_each do |user|
+      Notification.create(reason: :updated, action_type: rent.class.name,
                           action_id: rent.id, from: rent.user, to: user)
     end
   end
