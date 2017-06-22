ab2615af8bb3e539650a89dd21be75890b9505e0
mtrucco <truccomarcos@gmail.com>
2017-06-22 16:10:38 -0300

    Changed reason for rent notification to updated

--- app/workers/notify_when_rent_over_worker.rb
+++ app/workers/notify_when_rent_over_worker.rb
@@ -10,7 +10,7 @@ class NotifyWhenRentOverWorker
 
   def create_notifications(rent)
     rent.book.users.each do |user|
-      Notification.create(reason: 3, action_type: 'end',
+      Notification.create(reason: :updated, action_type: 'end',
                           action_id: rent.id, from: rent.user, to: user)
     end
   end
