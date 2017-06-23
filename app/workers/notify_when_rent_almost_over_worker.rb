b1e77bd9923a0f45a987b1920ca7b5b30af4673b
mtrucco <truccomarcos@gmail.com>
2017-06-23 14:10:30 -0300

    Added NotifyWhenRentAlmostOverWorker

--- app/workers/notify_when_rent_almost_over_worker.rb
+++ app/workers/notify_when_rent_almost_over_worker.rb
@@ -1,15 +1,15 @@
-class NotifyWhenRentOverWorker
+class NotifyWhenRentAlmostOverWorker
   include Sidekiq::Worker
 
   def perform
-    rents = Rent.where(to: Time.zone.today, returned_at: nil)
+    rents = Rent.where(to: Time.zone.today + 3, returned_at: nil)
     rents.find_each do |rent|
       create_notifications(rent)
     end
   end
 
   def create_notifications(rent)
-    Notification.create(reason: :updated, body: 'The rent is expired',
+    Notification.create(reason: :updated, body: 'The rent is about to expire',
                         action_type: rent.class.name, action_id: rent.id,
                         from: rent.user, to: rent.user)
   end
