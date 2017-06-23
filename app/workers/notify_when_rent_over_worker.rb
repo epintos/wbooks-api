f5bd550d9addda4c498e32db86aeb4d076664ab3
mtrucco <truccomarcos@gmail.com>
2017-06-23 13:48:41 -0300

    Edited worker, now it sends a message 3 days before the rent is due

--- app/workers/notify_when_rent_over_worker.rb
+++ app/workers/notify_when_rent_over_worker.rb
@@ -2,16 +2,15 @@ class NotifyWhenRentOverWorker
   include Sidekiq::Worker
 
   def perform
-    rents = Rent.where(to: Time.zone.today)
+    rents = Rent.where(to: Time.zone.today + 3)
     rents.find_each do |rent|
-      create_notifications(rent)
+      create_notifications(rent) if rent.returned_at.nil?
     end
   end
 
   def create_notifications(rent)
-    rent.book.users.find_each do |user|
-      Notification.create(reason: :updated, action_type: rent.class.name,
-                          action_id: rent.id, from: rent.user, to: user)
-    end
+    Notification.create(reason: :updated, body: 'The rent is about to expire',
+                        action_type: rent.class.name, action_id: rent.id,
+                        from: rent.user, to: rent.user)
   end
 end
